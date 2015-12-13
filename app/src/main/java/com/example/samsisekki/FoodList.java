4e8d396545fac9dc90fc1ef9fab575ee2e222f0d
donghyuk <donghyuk92@naver.com>
2015-12-13 10:31:27 +0900

    SQLite , MySQL 연동

--- app/src/main/java/com/example/samsisekki/FoodList.java
+++ app/src/main/java/com/example/samsisekki/FoodList.java
@@ -5,10 +5,15 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.samsisekki.parsing.parsing;
@@ -21,20 +26,44 @@ import java.util.List;
 /**
  * Created by slave on 2015-11-17.
  */
-public class FoodList extends ListActivity {
+public class FoodList extends AppCompatActivity {
 
+    TextView tv01;
+    ListView foodlist;
+    String[] values = new String[] { "후라이드치킨", "양념치킨", "간장치킨", "퓨전치킨", "마늘치킨" };
+    RatingBar rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.foodlist);
 
-        String[] values = new String[] { "후라이드치킨", "양념치킨", "간장치킨", "퓨전치킨", "마늘치킨" };
-        // use your custom layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                R.layout.foodlist, R.id.listview, values);
-        setListAdapter(adapter);
+        CustomList adapter = new CustomList(this);
+        foodlist = (ListView) findViewById(R.id.foodlist);
+        foodlist.setAdapter(adapter);
+        foodlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), values[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+        /**
+        tv01 = (TextView) findViewById(R.id.text1);
+        rating = (RatingBar) findViewById(R.id.ratingBar1);
+        rating.setStepSize((float) 0.5);        //별 색깔이 1칸씩줄어들고 늘어남 0.5로하면 반칸씩 들어감
+        rating.setRating((float) 2.5);      // 처음보여줄때(색깔이 한개도없음) default 값이 0  이다
+        rating.setIsIndicator(false);           //true - 별점만 표시 사용자가 변경 불가 , false - 사용자가 변경가능
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+                tv01.setText("평점 : " + rating);
+            }
+        });
+    }
+**/
+    /**
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         String item = (String) getListAdapter().getItem(position);
@@ -42,9 +71,23 @@ public class FoodList extends ListActivity {
         Intent intent = new Intent(this, parsing.class);
         startActivity(intent);
     }
+    **/
     public void move(View v) {
         Intent intent = new Intent(this, parsing.class);
         intent.putExtra("name","치킨");
         startActivity(intent);
+    }
+    public class CustomList extends ArrayAdapter<String> {
+        private final Activity context;
+        public CustomList(Activity context) {
+            super(context, R.layout.listitem, values);
+            this.context = context;
+        }
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            View rowView = inflater.inflate(R.layout.listitem,null,true);
+            return rowView;
+        }
     }
 }
--- app/src/main/java/com/example/samsisekki/History.java
+++ app/src/main/java/com/example/samsisekki/History.java
@@ -1,19 +1,39 @@
 package com.example.samsisekki;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.user.menu4u.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  *
  */
 public class History extends Fragment {
+
+    ArrayList<String> arrlist = null;
+    ArrayList<String> arr_id_list = null;
+    SQLiteDatabase database;
+    String dbName = "test_db_name";
+    private ListView    m_ListView;
+    private CustomAdapter   m_Adapter;
+
 
     public static History newInstance(){
         History fragment = new History();
@@ -25,12 +45,86 @@ public class History extends Fragment {
         // Required empty public constructor
     }
 
+        @Override
+        public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState){
+            // Inflate the layout for this fragment
+            final View rootView = inflater.inflate(R.layout.history_layout,container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.history_layout, container, false);
+
+            // 커스텀 어댑터 생성
+            m_Adapter = new CustomAdapter();
+
+            // Xml에서 추가한 ListView 연결
+            m_ListView = (ListView) rootView.findViewById(R.id.historylist);
+
+            // ListView에 어댑터 연결
+            m_ListView.setAdapter(m_Adapter);
+
+            // ListView에 아이템 추가
+            m_Adapter.add("하스스톤");
+            m_Adapter.add("몬스터 헌터");
+            m_Adapter.add("디아블로");
+            m_Adapter.add("와우");
+            m_Adapter.add("리니지");
+            m_Adapter.add("안드로이드");
+            m_Adapter.add("아이폰");            m_Adapter.add("하스스톤");
+            m_Adapter.add("몬스터 헌터");
+            m_Adapter.add("디아블로");
+            m_Adapter.add("와우");
+            m_Adapter.add("리니지");
+            m_Adapter.add("안드로이드");
+            m_Adapter.add("아이폰");            m_Adapter.add("하스스톤");
+            m_Adapter.add("몬스터 헌터");
+            m_Adapter.add("디아블로");
+            m_Adapter.add("와우");
+            m_Adapter.add("리니지");
+            m_Adapter.add("안드로이드");
+            m_Adapter.add("아이폰");
+
+        m_ListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int arg2, long arg3) {
+
+                Toast.makeText(getActivity(), "삭제?", Toast.LENGTH_LONG).show();
+                AlertDialog.Builder alertDlg = new AlertDialog.Builder(rootView.getContext());
+                alertDlg.setTitle("삭제?");
+                alertDlg.setPositiveButton("넵", new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    //    String position = get(selectedPos);
+                    //    final String sql = "delete from test_table where id = "+ position;
+                        dialog.dismiss();
+                    //    database.execSQL(sql);
+                    }
+                });
+
+
+                alertDlg.setNegativeButton( "No!" , new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick( DialogInterface dialog, int which ) {
+                     /**
+                        String position = arr_id_list.get(selectedPos);
+                        dialog.dismiss();
+                        Log.i("test", "1");
+                        Intent intent = new Intent(second.this, updatedb.class);
+                        intent.putExtra("p_id", position);
+                        Log.i("test", "2");
+                        startActivity(intent);
+                      **/
+                    }
+                });
+
+                alertDlg.setMessage("안지웠어요!");
+                alertDlg.show();
+                return false;
+            }
+        });
+        return rootView;
     }
     @Override
     public void onStart() {
@@ -82,5 +176,54 @@ public class History extends Fragment {
         Log.d(this.getClass().getSimpleName(), "onSaveInstanceState()");
         super.onSaveInstanceState(outState);
     }
+    public void selectData(){
+        String sql = "select * from test_table";
+        Cursor result = database.rawQuery(sql, null);
+        result.moveToFirst();
+        while(!result.isAfterLast()){
+            arr_id_list.add(result.getString(0));
+            arrlist.add(result.getString(1));
+            result.moveToNext();
+        }
+        result.close();
+    }
 
+    /**
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        final Integer selectedPos = i;
+        AlertDialog.Builder alertDlg = new AlertDialog.Builder(view.getContext());
+        alertDlg.setTitle(R.string.alert_title_question);
+        Log.i("test", "1");
+        alertDlg.setPositiveButton( R.string.button_yes, new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String position = arr_id_list.get(selectedPos);
+                final String sql = "delete from test_table where id = "+ position;
+                dialog.dismiss();
+                Log.i("test", "onclick");
+                database.execSQL(sql);
+            }
+        });
+
+        alertDlg.setNegativeButton( R.string.button_no, new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick( DialogInterface dialog, int which ) {
+                String position = arr_id_list.get(selectedPos);
+                dialog.dismiss();
+                Log.i("test", "1");
+                Intent intent = new Intent(second.this, updatedb.class);
+                intent.putExtra("p_id", position);
+                Log.i("test", "2");
+                startActivity(intent);
+            }
+        });
+
+        alertDlg.setMessage(R.string.alert_msg_delete);
+        alertDlg.show();
+        return false;
+
+    }**/
 }
